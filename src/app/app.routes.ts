9a92ade80bbec798afc815170f9c4b1ba51f42c8
Ali najafian <alinajafian580@gmail.com>
2024-06-10 13:56:07 +0330

    added dashboard Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { ErrorComponentComponent } from './pages/error-component/error-component.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: SignInComponent},
+    {path: 'Dashboard', component: DashboardComponent},
     {path: '**', component: ErrorComponentComponent}
 ];
