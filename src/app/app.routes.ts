ed95d5a7e907dc5c2f607b180e7e8af2852161f3
Ali najafian <alinajafian580@gmail.com>
2024-06-10 14:28:16 +0330

    dashboard got routeGuarded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { ErrorComponentComponent } from './pages/error-component/error-component.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RouteGuardService } from './service/route-guard/route-guard.service';
 
 export const routes: Routes = [
-    {path: '', component: SignInComponent},
-    {path: 'dashboard', component: DashboardComponent},
+    {path: '', redirectTo: 'signin', pathMatch: 'full'},
+    {path: 'signin', component: SignInComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [RouteGuardService]},
     {path: '**', component: ErrorComponentComponent}
 ];
