f55308cbba2b76c0f1ad944378596d99f3284965
Ali najafian <alinajafian580@gmail.com>
2024-06-10 14:28:03 +0330

    RouteGuard added

---
+++ src/app/service/route-guard/route-guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { UserDataService } from '../user-data-service/user-data.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteGuardService implements CanActivate {
+
+  constructor(private userDataService: UserDataService,
+    private route: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      if (this.userDataService.getUsername() != '') {
+        return true;
+      } else {
+        this.route.navigate(['signin'])
+        return false;
+      }
+
+    }
+}
