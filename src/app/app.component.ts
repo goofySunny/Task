7e1068df11b242ca92f07703ecf40770bc1ca93f
Ali najafian <alinajafian580@gmail.com>
2024-06-10 15:29:49 +0330

    side nav added to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { SideNavComponent } from './pages/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
