ad2b2f27bc7b95df31d8b36cc089799d4ed2f07a
Ali najafian <alinajafian580@gmail.com>
2024-06-10 16:09:17 +0330

    added patient list component and routed it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { ErrorComponentComponent } from './pages/error-component/error-component.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RouteGuardService } from './service/route-guard/route-guard.service';
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'signin', pathMatch: 'full'},
     {path: 'signin', component: SignInComponent},
+    {path: 'patients', component: PatientListComponent, canActivate: [RouteGuardService]},
     {path: 'dashboard', component: DashboardComponent, canActivate: [RouteGuardService]},
     {path: '**', component: ErrorComponentComponent}
 ];
