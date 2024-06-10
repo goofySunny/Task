2c73c4c0d10cab97496919e065bdb8ea4b7b5805
Ali najafian <alinajafian580@gmail.com>
2024-06-10 12:27:57 +0330

    updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { ErrorComponentComponent } from './pages/error-component/error-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: SignInComponent},
+    {path: '**', component: ErrorComponentComponent}
+];
