05359fc8a3b9196c9eb6f1f7df9c48d10dcabe9c
Ali najafian <alinajafian580@gmail.com>
2024-06-10 13:51:51 +0330

    Sign in component set

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-
+  togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    const toggleIcon = document.getElementById('my-image') as HTMLImageElement;
+  
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      toggleIcon.src = "assets/Disable.svg";
+    } else {
+      passwordInput.type = 'password';
+      toggleIcon.classList.remove('fa-eye-slash');
+      toggleIcon.src = 'assets/Enable.svg';
+    }
+  }
 }
