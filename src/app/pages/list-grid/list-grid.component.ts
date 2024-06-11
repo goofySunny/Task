cf8c99697a02f50a76a0a2993ac39e35baa84d5d
Ali najafian <alinajafian580@gmail.com>
2024-06-11 10:49:15 +0330

    Improvements

--- src/app/pages/list-grid/list-grid.component.ts
+++ src/app/pages/list-grid/list-grid.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { Patient } from '../patient-list/patient-list.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list-grid',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './list-grid.component.html',
   styleUrl: './list-grid.component.scss'
 })
 export class ListGridComponent {
+  id = 1;
+
+  patients: Patient[] = [
+    { id: this.id++, name: "علی", description: "مریض", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "حسین", description: "مشکل تنفسی", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "نیما", description: "autism", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "جیمی", description: "اعتماد بنفس", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "جیمی", description: "اعتماد بنفس", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "جیمی", description: "اعتماد بنفس", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "جیمی", description: "اعتماد بنفس", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+    { id: this.id++, name: "جیمی", description: "اعتماد بنفس", phoneNumber: "09188181128", visitDate: new Date("1380/12/12") },
+  ]
 
   elements = document.getElementsByClassName("column") as HTMLCollectionOf<HTMLElement>;
   i = 0;
