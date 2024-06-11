4ec6593c2e3f8996daf8fd99f269aeece858d910
Ali najafian <alinajafian580@gmail.com>
2024-06-11 10:33:25 +0330

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { ErrorComponentComponent } from './pages/error-component/error-component
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RouteGuardService } from './service/route-guard/route-guard.service';
 import { PatientListComponent } from './pages/patient-list/patient-list.component';
+import { ListGridComponent } from './pages/list-grid/list-grid.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'signin', pathMatch: 'full'},
     {path: 'signin', component: SignInComponent},
+    {path: 'list-grid', component: ListGridComponent, canActivate: [RouteGuardService]},
     {path: 'patients', component: PatientListComponent, canActivate: [RouteGuardService]},
     {path: 'dashboard', component: DashboardComponent, canActivate: [RouteGuardService]},
     {path: '**', component: ErrorComponentComponent}
